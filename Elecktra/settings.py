fa0ec6238f41c51017ac89d9dcf7176f2f9ec40c
Pranjal Goyal <pranjalgoyal13@gmail.com>
2021-03-11 09:03:30 +0530

    Logged user are redirected to dashboard

--- Elecktra/settings.py
+++ Elecktra/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.mysql',
             'HOST' : '127.0.0.1',
-            'USER': 'shoot',
-            'PASSWORD': 'root',
+            'USER': 'root',
+            'PASSWORD': '',
             'NAME': 'elecktra',
         }
 }
