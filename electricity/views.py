64022fdd0c81b646ae5a8578dab278c165772303
nikhilkotiya <nikhilkotiya8@gmail.com>
2021-03-12 23:55:26 +0530

    Login form

--- electricity/views.py
+++ electricity/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import HttpResponse, redirect, render
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth.decorators import login_required 
-
-from electricity.forms import UserForm
+from django.contrib.auth.forms import UserCreationForm
 from electricity.models import electricity
 # Create your views here.
 
@@ -13,20 +15,30 @@ def index(request):
 
 
 def registeruser(request):
-    registeredcheck = False
-    if(request.method == 'POST'):
-        user_form = UserForm(data=request.POST)
-        if user_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.is_active = True
-            user.save()
-            registeredcheck = True
-            login(request,user)
-            return redirect('/')
-        else:
-            error={"error":True}
-            return render(request,"login.html",error)
+    if request.POST.get("form_two"):
+            if not User.objects.filter(username=request.POST["username"]).exists():
+                if request.POST["password"]==request.POST["rpassword"]:
+                    data=User()
+                    data.password=make_password(request.POST["password"])
+                    data.first_name=request.POST["first_name"]
+                    data.last_name=request.POST["last_name"]
+                    data.username=request.POST["username"]
+                    data.email = request.POST["email"]
+                    data.is_active = True
+                    print("user saved")
+                    data.save()
+                    
+                    return redirect('/login/')
+                else:
+                    context="Password confirmation doesn't match Password"
+                    print("wrong password")
+                    return render(request,"login.html",{'errorP':context})
+
+            else:
+                context="Email Already Exist"
+                return render(request,"login.html",{'errorE':context})
+                
+
     else:
         return render(request,"login.html")
 
@@ -49,6 +61,38 @@ def loginuser(request):
     else:
         return render(request, 'login.html')
 
+def forgotp(request):
+        if(request.method=="POST"):
+            if  User.objects.filter(username=request.POST["username"]).exists():
+                if User.objects.filter(first_name= request.POST["first_name"]).exists():
+                    if User.objects.filter(last_name=request.POST["last_name"]).exists():
+                        data=User(request.POST["username"])
+                        
+                        if request.POST["password"]==request.POST["rpassword"]:
+                            data.first_name=request.POST["first_name"]
+                            data.last_name=request.POST["last_name"]
+                            data.username=request.POST["username"]
+                            data.password=make_password(request.POST["password"])
+                            data.is_active = True
+                            print("user saved")
+                            data.save()
+                            return redirect('/')
+                        else:
+                            context="Password confirmation doesn't match Password"  
+                            return render(request,'forgotpassword.html' ,{'ErrorFP':context})
+                    else:
+                        context="Wrong Surname"  
+                        return render(request,'forgotpassword.html' ,{'ErrorWs':context})
+                else:
+                    context="Wrong First Name"
+                    return render(request,'forgotpassword.html', {'ErrorFP':context})
+            else:
+                context="Wrong Email"  
+                return render(request,'forgotpassword.html' ,{'ErrorEmail':context})
+                
+        else:
+            print("bc")
+            return render(request,'forgotpassword.html')
 def logoutuser(request):
     logout(request)
     return redirect('/')
