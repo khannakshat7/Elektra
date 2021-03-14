c6455e912f429fc0847eeeaf1d6318bda41f9e07
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-03-14 12:19:54 +0530

    Updated settings.py file back to previous version

--- Elecktra/settings.py
+++ Elecktra/settings.py
@@ -77,12 +77,11 @@ WSGI_APPLICATION = 'Elecktra.wsgi.application'
 
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST' : 'localhost',
-            'USER': 'root',
+             'ENGINE': 'django.db.backends.mysql',
+            'HOST' : '127.0.0.1',
+            'USER': '',
             'PASSWORD': '',
-            'NAME': 'elektra',
-            'PORT': '3306'
+            'NAME': 'elecktra',
         }
 }
 
