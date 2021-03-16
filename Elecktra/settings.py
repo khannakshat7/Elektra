0b64257c16923a56a7716a9427e9a45c621d9952
siddharth <siddharthrandive000@gmail.com>
2021-03-17 02:01:49 +0530

    login and sign in page design in html and css

--- Elecktra/settings.py
+++ Elecktra/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.mysql',
             'HOST' : '127.0.0.1',
-            'USER': '',
-            'PASSWORD': '',
+            'USER': 'root',
+            'PASSWORD': 'root',
             'NAME': 'elecktra',
         }
 }
