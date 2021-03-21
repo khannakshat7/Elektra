edab7e203e4ec0c0c198dfe61d906a2f91904313
Priyanka7011 <pk3201134@gmail.com>
2021-03-21 19:42:25 +0530

    #42 icons made clickable

--- Elecktra/settings.py
+++ Elecktra/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',
             'HOST' : '127.0.0.1',
             'USER': 'root',
-            'PASSWORD': 'papa1602',
+            'PASSWORD': '',
             'NAME': 'elektra',
         }
 }
