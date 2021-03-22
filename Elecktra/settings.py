bc8dd9591ea89d6ec5ee6d2444b6765532a58a02
Aditya Raj <aditya201551@gmail.com>
2021-03-22 21:45:52 +0530

    added social-auth

--- Elecktra/settings.py
+++ Elecktra/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'Elecktra.wsgi.application'
 
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'HOST' : '127.0.0.1',
-            'USER': '',
-            'PASSWORD': '',
-            'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
-        },
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'USER': '',
+        'PASSWORD': '',
+        'NAME': 'elecktra',
+    }
 }
 
 
