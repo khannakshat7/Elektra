49ab31bc68f1b0243d7edf03e129e7342acbeda1
arpit456jain <111arpit1@gmail.com>
2021-04-14 22:24:47 +0530

    moved js code from showpassword to main.js

--- static/main.js
+++ static/main.js
@@ -4,6 +4,7 @@
  * Author: BootstrapMade.com
  * License: https://bootstrapmade.com/license/
  */
+
 !(function ($) {
   "use strict";
 
@@ -247,3 +248,38 @@ win.scroll(function (event) {
   });
 
 });
+//for show pass word in signup
+function showpasforsignup()
+{
+  
+  var password=document.getElementById("psw");
+  var password2=document.getElementById("psw2");
+  var x=document.getElementById("box1").checked;
+  if(x==true)
+  {
+    password.type="text";
+    password2.type="text";
+    
+  }
+  else
+  {
+    password.type="password";
+    password2.type="password";
+    
+  }
+}
+  //for show password in login
+  function showpasforlogin()
+{
+  var password=document.getElementById("loginpsw2");
+  var x=document.getElementById("box2").checked;
+  if(x==true)
+  {
+    password.type="text";
+  }
+  else
+  {
+    password.type="password";    
+  }
+}
+
