b38ac6c01f619966625ef7959cb43c43c0c8c914
kumarishalini6 <kumarishalu1206@gmail.com>
2021-05-23 09:50:25 -0800

    shgc

--- Elecktra/settings.py
+++ Elecktra/settings.py
@@ -89,9 +89,9 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.mysql',
         'HOST': '127.0.0.1',
-        'USER': '',
+        'USER': 'root',
         'PASSWORD': '',
-        'NAME': 'elecktra',
+        'NAME': 'crud',
     }
 }
 
