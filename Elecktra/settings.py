380353ca6bc6ff344bac75db6f846a2141f3f52f
kumarishalini6 <kumarishalu1206@gmail.com>
2021-05-24 22:56:29 -0800

    setting.py

--- Elecktra/settings.py
+++ Elecktra/settings.py
@@ -88,9 +88,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': '127.0.0.1',
-        'USER': 'root',
+        'USER': '',
         'PASSWORD': '',
-        'NAME': 'crud',
+        'NAME': 'elecktra',
     }
 }
 
