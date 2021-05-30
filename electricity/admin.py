f0813f2c76d1add89a18efe6eb5fdb9e617adc53
arpit456jain <111arpit1@gmail.com>
2021-05-30 15:38:46 +0530

    added download csv feature for contact and elecktricity

--- electricity/admin.py
+++ electricity/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from electricity.models import electricity,Contact,FeedBack
+from electricity.models import electricity,Contact,FeedBack,electricity
 from import_export.admin import ImportExportModelAdmin
 # Register your models here.
-admin.site.register(electricity)
+# admin.site.register(electricity)
 #Registered Contact model
-admin.site.register(Contact)
+# admin.site.register(Contact)
 # admin.site.register(FeedBack)
 
 @admin.register(FeedBack)
 class FeedBackAdmin(ImportExportModelAdmin):
+    pass
+@admin.register(Contact)
+class ContactAdmin(ImportExportModelAdmin):
+    pass
+@admin.register(electricity)
+class UserAdmin(ImportExportModelAdmin):
     pass
